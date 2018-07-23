8836a50c5ab5bcaf2e375667662dd8f7cca0541c
Kirill <k_kobzev@mail.ru>
2018-07-24 00:06:40 +0300

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<form action="auth.php" method="POST">
+	<br>
+	<input type="text" name="login" placeholder="Login"><br><br>
+	<input type="text" name="password" placeholder="Password"><br><br>
+	<button type="submit" name="button">Авторизоваться</button><br>
+</form>
+
+<a href="signup.php">Страница регистрации</a>
