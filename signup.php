c2641bc42119d4428d341323720f8772e2e77651
Kirill <k_kobzev@mail.ru>
2018-07-24 00:06:27 +0300

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,9 @@
+<meta charset="utf-8">
+<form action="reg.php" method="POST">
+	<br>
+	<input type="text" name="login" placeholder="Login"><br><br>
+	<input type="text" name="password" placeholder="Password"><br><br>
+	<button type="submit" name="button">Зарегистрироваться</button><br>
+</form>
+
+<a href="signin.php">Страница авторизации</a>
